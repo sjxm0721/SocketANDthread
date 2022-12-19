2af18a517f7589f788c0fc6939dac43afe6a679f
l <2472077228@qq.com>
2022-12-18 18:57:52 -0800

    简单的多线程 + socket

--- Server.cpp
+++ Server.cpp
@@ -3,8 +3,48 @@
 #include<string.h>
 #include<arpa/inet.h>
 #include<iostream>
+#include<pthread.h>
 
 using namespace std;
+
+//信息结构体
+struct SockInfo
+{
+    sockaddr_in addr;
+    int fd;
+};
+
+SockInfo infos[512];
+
+void* working(void* arg) {
+    SockInfo* pinfo=(SockInfo*)arg;
+    //连接建立成功，打印客户端的IP和端口信息
+    char ip[32];
+    cout << "客户端的IP " << inet_ntop(AF_INET, &pinfo->addr.sin_addr.s_addr, ip, sizeof(ip)) << " 端口: "
+         << ntohs(pinfo->addr.sin_port) << endl;
+
+    //通信
+
+    while (1) {
+        char buff[1024];
+        int len = recv(pinfo->fd, buff, sizeof(buff), 0);
+        if (len > 0) {
+            cout << "client say: " << buff;
+            send(pinfo->fd, buff, len, 0);
+        } else if (len == 0) {
+            cout << "客户端已经断开了连接..." << endl;
+            break;
+        } else {
+            perror("recv");
+            break;
+        }
+    }
+    //关闭文件描述符
+    close(pinfo->fd);
+    pinfo->fd=-1;
+
+    return NULL;
+}
 
 int main()
 {
@@ -36,45 +76,39 @@ int main()
         return -1;
     }
 
-    //阻塞并等待客户端的连接
-    sockaddr_in caddr;
-    int addrlen=sizeof(sockaddr_in);
-    int cfd=accept(fd,(sockaddr*)&caddr,(socklen_t*)&addrlen);
-    if(cfd==-1)
+    //初始化结构体数组
+    int max=sizeof(infos)/sizeof(infos[0]);
+    for(int i=0;i<max;i++)
     {
-        perror("accept");
-        return -1;
+        memset(&infos[i],0,sizeof(infos[i]));
+        infos[i].fd=-1;
     }
 
-    //连接建立成功，打印客户端的IP和端口信息
-    char ip[32];
-    cout<<"客户端的IP "<<inet_ntop(AF_INET,&caddr.sin_addr.s_addr,ip,sizeof(ip))<<" 端口: "<<ntohs(caddr.sin_port)<<endl;
-
-    //通信
-
+    //阻塞并等待客户端的连接
+    int addrlen=sizeof(sockaddr_in);
     while(1)
     {
-        char buff[1024];
-        int len=recv(cfd,buff,sizeof(buff),0);
-        if(len>0)
+        SockInfo* pinfo;
+        for(int i=0;i<max;i++)
         {
-            cout<<"client say: "<<buff;
-            send(cfd,buff,len,0);
+            if(infos[i].fd==-1)
+            {
+                pinfo=&infos[i];
+                break;
+            }
         }
-        else if(len==0)
-        {
-            cout<<"客户端已经断开了连接..."<<endl;
+        int cfd = accept(fd, (sockaddr *) &pinfo->addr, (socklen_t *) &addrlen);
+        if (cfd == -1) {
+            perror("accept");
             break;
         }
-        else
-        {
-            perror("recv");
-            break;
-        }
+        pinfo->fd=cfd;
+        //创建子线程
+        pthread_t tid;
+        pthread_create(&tid,NULL,working,pinfo);
+        pthread_detach(tid);
     }
-
     //关闭文件描述符
     close(fd);
-    close(cfd);
     return 0;
 }
